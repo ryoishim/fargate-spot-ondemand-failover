dcc0c5d490ce0a3d1bb8385d0ab1d9903698b2d4
Ishimoto <ryoishim@909c4ace68cb.ant.amazon.com>
2021-04-26 22:38:12 +0900

    Update function code

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,17 +2,25 @@ import json
 import boto3
 import os
 import logging
+import requests
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 client = boto3.client('ecs')
+slack_webhook = os.environ['SLACK_WEBHOOK']
 
-def get_desired_capacity(cluster, base_service):
-    response = client.describe_services(
-        cluster=cluster,
-        services=[
-            base_service,
-        ]
-    )
+def get_desired_capacity(cluster, service):
+    try:
+        response = client.describe_services(
+            cluster=cluster,
+            services=[
+                service,
+            ]
+        )
+    except ClientError as e:
+        logger.error(e.response['Error']['Code'])
+        logger.error(e.response['Error']['Message'])
+        raise e
     desired_capacity = response['services'][0]['desiredCount']
     running_capacity = response['services'][0]['desiredCount']
     return desired_capacity - running_capacity
@@ -25,7 +33,16 @@ def update_desired_capacity(cluster, bkup_service, bkup_expected_desired_capacit
     )
     return 0
 
+def put_message_to_slack():
+    response = requests.post(
+        slack_webhook
+    )
+    #レスポンスオブジェクトのjsonメソッドを使うと、
+    #JSONデータをPythonの辞書オブジェクトを変換して取得できる。
+    logger.info(response)
+
 def lambda_handler(event, context):
+    logger.info(event)
     cluster      = os.environ['ECS_CLUSTER']
     base_service = os.environ['ECS_SERVICE_BASE']
     bkup_service = os.environ['ECS_SERVICE_BACKUP']
@@ -40,4 +57,9 @@ def lambda_handler(event, context):
     else :
         logger.info('expected_bkup_desired_capacity is 0, desired_capacity is filled.')
         
-    return 0
+    put_message_to_slack()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
